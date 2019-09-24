21839fe72df722b894cf68f739a706e5a8d4dc4d
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-09-24 17:11:03 -0400

    set up main.storyboard

---
+++ Pursuit-Core-iOS-UserDefaults-Lab/Pursuit-Core-iOS-UserDefaults-Lab/HoroscopeModel.swift
@@ -0,0 +1,9 @@
+//
+//  HoroscopeModel.swift
+//  Pursuit-Core-iOS-UserDefaults-Lab
+//
+//  Created by Krystal Campbell on 9/24/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import Foundation
