747bffaced8c5c48a3d98a3c8df8481fa5ba51af
Ahad Islam <ahad.islam624@gmail.com>
2019-12-24 15:53:27 -0500

    REDOING EVERYTHING.

--- userDefaultslab/userDefaultslab/SceneDelegate.swift
+++ userDefaultslab/userDefaultslab/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = scene
-        window?.rootViewController = UINavigationController(rootViewController: HoroscopeViewController())
+        window?.rootViewController = ViewController()
 //        window?.rootViewController = HoroscopeViewController()
         window?.makeKeyAndVisible()
         // Need to study window and scene delegates.
---
+++ userDefaultslab/userDefaultslab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  userDefaultslab
+//
+//  Created by Ahad Islam on 11/19/19.
+//  Copyright © 2019 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+
